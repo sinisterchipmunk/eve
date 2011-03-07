b697430cd1e99e18f79feb338e9e8c1e099808f6
Emalee <rs.of.chrs@gmail.com>
2011-03-06 23:43:01 -0800

    requires_trust page can be overridden

--- lib/eve/deprecation.rb
+++ lib/eve/deprecation.rb
@@ -1,4 +1,3 @@
 ActiveSupport::Deprecation.deprecate_methods(Eve::Trust::ControllerHelpers, {
-  :require_trust => "The EVE IGB no longer accepts trust headers. Please use <%=request_trust%> in your view, instead.",
   :prefer_trust  => "The EVE IGB no longer accepts trust headers. Please use <%=request_trust%> in your view, instead."
 })
